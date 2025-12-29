30cf7a55091a1fa8deb97b8e005d214290e23993
pebloop <pebloop@outlook.com>
2025-12-29 16:07:46 +0100

    init project

--- src/routes/grammar/sentence/+page.svelte
+++ src/routes/grammar/sentence/+page.svelte
@@ -8,33 +8,14 @@
 
 			<h2 class="text-3xl">Sentence</h2>
 			<hr/>
-			<p>Write your first sentences in sealang</p>
+			<p>Write your first sentences in skylang</p>
 			<br>
 
 			<h3 class="text-2xl">The sentence structure</h3>
 			<hr/>
 
 			<p>
-				A sentence in sealang always start with the verb and finish with a ".", written "<span class="font-sealang">??</span>".
-				<br/>
-				To differentiate a word usage, we use something called a "particle". For example the verb "<span class="font-sealang">s</span>" (live)
-				become a noun with the noun particle  "<span class="font-sealang">D</span>" : "<span class="font-sealang">sD</span>" (alive)
-				<br/>
-				Verbs use 3 different particles to conjugate themself, past, present and futur : "<span class="font-sealang">E</span>", "<span class="font-sealang">A</span>" and "<span class="font-sealang">C</span>".
-				<br/>
-				This way you now can write the most basic sentences :
-			</p>
-			<div class="flex flex-row items-center justify-center">
-				<img src="/grammar/i_have_a_fish.png" alt="I have a fish structure" class="w-1/2 h-auto mt-4 mb-4" />
-			</div>
 
-			<h3 class="text-2xl">The <span class="font-sealang">V</span> particle</h3>
-			<hr/>
-
-			<p>
-				Another of the first particle to learn, is the <span class="font-sealang">V</span>.
-				Placed right after the verb, it turn the sentence into negation. For example the previous sentence :
-				"<span class="font-sealang">eA?fD??</span>"(I have a fish) become "<span class="font-sealang">eA?V ?fD??</span>" (I have no fish)
 			</p>
 
 		</div>
