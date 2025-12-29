f6f9ccad64aee9f0e80495fb3627d7c4488aaac9
pebloop <pebloop@outlook.com>
2025-12-29 19:25:14 +0100

    Explain alphabet basics

--- src/routes/grammar/alphabet/+page.svelte
+++ src/routes/grammar/alphabet/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+</script>
+
 <div class="p-4 w-full h-full flex flex-col items-center justify-center">
 	<div class="w-1/2">
 		<a href="/grammar" class="text-blue-500 hover:text-blue-700">&leftarrow; grammar</a>
@@ -16,12 +19,62 @@
 				<img src="/grammar/keys.png" alt="language keys" class="w-1/2 h-auto mt-4 mb-4" />
 			</div>
 
+			<h3 class="text-2xl">Symbols formations</h3>
+			<hr/>
+
+			<p>
+				From those 9 keys are formed the different symbols, each symbol representing a whole word.
+				Let's take the example of the word "skylang" itself :
+			</p>
+			<div class="flex flex-row items-center justify-center">
+				<img src="/grammar/skylang_symbol.png" alt="skylang symbol" class="w-1/2 h-auto mt-4 mb-4" />
+			</div>
+
+			<p>
+				If you look closely you can identify 4 keys inside the symbol :
+			</p>
+			<div class="flex flex-row items-center justify-center">
+				<img src="/grammar/skylang_key_1.png" alt="skylang symbol key 1" class="w-1/2 h-auto mt-4 mb-4" />
+			</div>
+			<p>
+				The ground key at the top, meaning that the word is a noun.
+			</p>
+
+			<div class="flex flex-row items-center justify-center">
+				<img src="/grammar/skylang_key_2.png" alt="skylang symbol key 2" class="w-1/2 h-auto mt-4 mb-4" />
+			</div>
+			<p>
+				The sky key in the center.
+			</p>
+
+			<div class="flex flex-row items-center justify-center">
+				<img src="/grammar/skylang_key_3.png" alt="skylang symbol key 3" class="w-1/2 h-auto mt-4 mb-4" />
+			</div>
+			<p>
+				The sun key in the center, fused with the sky key. Here it means the mouth, because the sun key
+				can also represent the mouth.
+			</p>
+
+			<div class="flex flex-row items-center justify-center">
+				<img src="/grammar/skylang_key_4.png" alt="skylang symbol key 4" class="w-1/2 h-auto mt-4 mb-4" />
+			</div>
+			<p>
+				And finally the ground key at the bottom.
+			</p>
+
+			<br/>
+
+			<p>
+				The sky key and the ground key together means the "country of the sky", fused ith the mouth key
+				it becomes "the language of the country of the sky", and the ground key at the top indicates that it's a noun.
+			</p>
+
 		</div>
 	</div>
 </div>
 
 <style>
-	.font-sealang {
-		font-family: 'sealang', sans-serif;
+	.font-skylang {
+		font-family: 'skylang', sans-serif;
 	}
 </style>
