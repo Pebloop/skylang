544d7960bc2d510bd2875766814d5567715208e6
pebloop <pebloop@outlook.com>
2025-12-31 02:52:03 +0100

    Add fly page

---
+++ src/routes/dictionary/fly/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+</script>
+
+<div class="p-4 w-full h-full flex flex-col items-center justify-center">
+    <div class="w-1/2">
+        <a href="/dictionary" class="text-blue-500 hover:text-blue-700">&leftarrow; dictionary</a>
+        <div class="w-full bg-gray-100 p-4 rounded-lg shadow-lg mt-4">
+
+            <h2 class="text-3xl">To fly</h2>
+            <hr/>
+            <p>To move through the air.</p>
+            <br>
+
+            <h3 class="text-2xl">Composition</h3>
+            <hr/>
+
+            <p><span class="font-skylang">H</span> is composed of the following keys :</p>
+            <ul>
+                <li><span class="font-skylang">7</span> : verb / present tense marker</li>
+                <li><span class="font-skylang">6</span> : sky</li>
+            </ul>
+
+            <br/>
+            <h3 class="text-2xl">Pronunciation</h3>
+            <hr/>
+            <p>The pronunciation of <span class="font-skylang">H</span> is [ɸɔɑzɛ], the "sky" [ɸɔɑz] with the [ɛ] to turn it into a verb.</p>
+
+            <br/>
+            <h3 class="text-2xl">Conjugation</h3>
+            <hr/>
+            <p>The verb "to eat" is conjugated as follows :</p>
+            <ul>
+                <li>fly (present tense) : <span class="font-skylang">H</span> [ɸɔɑzɛ]</li>
+                <li>flew (past tense) : <span class="font-skylang">L</span> [ɸɔɑzɛɛ]</li>
+                <li>will fly (future tense) : <span class="font-skylang">M</span> [ɸɔɑzɛɔ]</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<style>
+    .font-skylang {
+        font-family: 'skylang', sans-serif;
+    }
+</style>
